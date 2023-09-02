6688adf1cf50ed7323ad1d1df1e647f28993bf28
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-09-02 18:52:35 +0200

    made the chating feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 function App() {
+
   return (
     <>
       <h1 className=" text-3xl">hello world</h1>
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,13 +1,14 @@
 import { Message } from "../pages/Chat"
-
+import { v4 as uuidv4 } from 'uuid';
 type Props = {
     messages: Message[]
+    userId: string
 }
 export default function Messages(props: Props) {
 
-    return <div className="w-[80vw] border-2 border-black h-[65vh] rounded-2xl px-5 pt-5 mt-10" >
+    return <div className="w-[80vw] border-2 border-black h-[65vh] rounded-2xl px-5 pt-5 mt-10 overflow-y-scroll" >
         {
-            props.messages.map(msg => <p className="text-xl"><span className="font-bold">{msg.sender} : </span> {msg.text}</p>)
+            props.messages.map(msg => <p className="text-xl" key={uuidv4()}><span className="font-bold">{msg.userId === props.userId ? "me" : msg.senderName} : </span> {msg.text}</p>)
         }
     </div>
 }
